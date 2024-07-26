515e4cd1cbac3e8a51cbcad1003fa7142b963b2b
Munish Goel <98271284+MG2356@users.noreply.github.com>
2024-07-26 22:13:58 +0530

    Update app.util.js

--- User/User/src/Utils/app.util.js
+++ User/User/src/Utils/app.util.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://food-23ir.onrender.com"
+export const API_URL = "https://food-cc8q.onrender.com"
 // export const API_URL = "http://localhost:7000"
 
 //https://food-23ir.onrender.com/getProduct
